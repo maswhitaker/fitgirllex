d002d2b87176a559df21ffb086dc0d827b2bd202
maswhitaker <masforwhi@yahoo.com>
2014-08-28 15:45:30 -0400

    starting parse application

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('The Iron Yard Rocks');
+Parse.initialize("CGiH8mU7FWqmYhm2HXL1KZ2yusLAYc6uLGLxWKOE", "pFMs3sr9uuLC8ITdi6mK2unnU4xVARJ97grozseD");
+
+var HomeView = Parse.View.extend({
+
+  template: _.template($("#home").html()),
+
+  initialize: function(){
+    $(".container").html(this.el);
+    this.render();
+  },
+
+  render: function(){
+    this.$el.html(this.template(this.model));
+  }
+});
+
+var AboutView = Parse.View.extend({
+  template: _.template($("#about").html()),
+
+  initialize: function(){
+    $(".container").html(this.el);
+    this.render();
+  },
+  render: function(){
+    this.$el.html(this.template(this.model));
+  }
+})
+
+new AboutView();
